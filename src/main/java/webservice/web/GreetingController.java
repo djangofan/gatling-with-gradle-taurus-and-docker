74a6b4401cc669a088abf92834de0c7f98849ce0
Apostolos Benisis <apostolos.benisis@hotmail.de>
2017-09-11 22:49:28 +0200

    Added files of finished tutorial

---
+++ src/main/java/webservice/web/GreetingController.java
@@ -0,0 +1,13 @@
+package webservice.web;
+
+import webservice.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public Greeting greeting() {
+        return new Greeting("Hello, World");
+    }
+}
